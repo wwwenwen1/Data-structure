284f1a7e03c5e986596764289209da81613046d2
dww <710241413@qq.com>
2023-09-10 21:15:29 +0800

    9.10完成链式队列的基本操作

--- Basic data Structure/queue/ListQueue(HeadNode).c
+++ Basic data Structure/queue/ListQueue(HeadNode).c
@@ -19,8 +19,8 @@ typedef struct Queue{
 int initQueue(LinkQueue Q){
     Q->front=(QNode*)malloc(sizeof(QNode));
     Q->rear=Q->front;
-    (*Q->front).next=NULL;
-    (*Q->front).data = 0;
+    Q->front->next=NULL;
+    Q->front->data = 0;
 
     return 0;
 }
@@ -30,20 +30,25 @@ int EnQueue(LinkQueue Q,int value){
     ptrNewNode =(QNode*)malloc(sizeof(QNode));
     ptrNewNode->data=value;
     ptrNewNode->next=NULL;
+    Q->rear->next=ptrNewNode;
     Q->rear=ptrNewNode;
-    (*Q->front).data++;
+    Q->front->data++;
 
     return 0;
 }
 //出队
 int DeQueue(LinkQueue Q){
+    if(Q->front->next==NULL){
+        printf("队列为空，无法出队.\n");
+        return 1;
+    }
     QNode* ptrTempNode;
     int value;
-    ptrTempNode=(*Q->front).next;
-    (*Q->front).next=(*Q->front->next).next;
-    (*Q->front).data--;
+    ptrTempNode=Q->front->next;
+    Q->front->next=Q->front->next->next;
     value=ptrTempNode->data;
     free(ptrTempNode);
+    Q->front->data--;
 
     return value;
 }
@@ -60,11 +65,33 @@ int GetHead(LinkQueue Q){
         printf("列表为空，无法获取队首元素.\n");
     }
     else 
-        return (*Q->front->next).data;
+        return Q->front->next->data;
 }
 int main(){
-    LinkQueue Q;
+    LinkQueue Q;            
+    /*
+    在C语言中，声明一个指针变量（例如 LinkQueue Q;）不会自动为该指针分配内存。
+    当你声明 LinkQueue Q; 时，实际上只是创建了一个指向 Queue 结构的指针，但这个指针没有指向任何有效的内存地址。
+    因此，你必须在使用这个指针之前手动分配内存来存储 Queue 结构。*/
+    Q = (LinkQueue)malloc(sizeof(struct Queue));
     initQueue(Q);
-    
+    EnQueue(Q,1);
+    EnQueue(Q,2);
+    EnQueue(Q,3);
+    EnQueue(Q,4);
+    EnQueue(Q,5);
+    printf("队列是否为空:%d\n",isEmpty(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("出队元素:%d\n",DeQueue(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("出队元素:%d\n",DeQueue(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("出队元素:%d\n",DeQueue(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("出队元素:%d\n",DeQueue(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("出队元素:%d\n",DeQueue(Q));
+    printf("队首元素:%d\n",GetHead(Q));
+    printf("队列是否为空:%d\n",isEmpty(Q));
     return 0;
 }
