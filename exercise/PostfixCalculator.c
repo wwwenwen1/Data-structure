8ae337eca076002720c48c19f6a5968c95c43aa0
dww <710241413@qq.com>
2023-09-15 17:56:14 +0800

    9.15完成逆波兰式计算算法

--- exercise/PostfixCalculator.c
+++ exercise/PostfixCalculator.c
@@ -4,7 +4,7 @@
 
 //直接把链栈的内容拿过来用
 typedef struct node{
-    char data;
+    int data;
     struct node* next;
 }SNode, *LinkStack;
 
@@ -31,7 +31,7 @@ int isEmpty(LinkStack L){
         return 0;
 }
 //
-int push(LinkStack* ptrL,char value){
+int push(LinkStack* ptrL,int value){
     SNode* NewNode;
     SNode* headNode;
     headNode=(*ptrL)->next;
@@ -47,7 +47,7 @@ int push(LinkStack* ptrL,char value){
     return 0;
 }
 //
-char pop(LinkStack* ptrL){
+int pop(LinkStack* ptrL){
     SNode* TopNode;
     char value;
     TopNode=(*ptrL)->next;              //取出第二个节点
@@ -57,14 +57,40 @@ char pop(LinkStack* ptrL){
     return value;
 }
 //
-char getTop(LinkStack S){
+int getTop(LinkStack S){
     char top;
     top=S->next->data;
     return top;                         //S是指向头节点的指针(头指针)，S->next是指向第二个节点的指针(真正开始存储数据的节点)，S->next->data是第二个节点的数据域
 }
 
-int calculateRPN(){
-
-
-
+int calculateRPN(char* RPN){//不用数组，直接用指针，因为数组是指针常量，不能修改数组的值
+    LinkStack S;
+    initStack(&S);
+    char ch;
+    int a,b;
+    while(*RPN!='\0'){
+        ch=*RPN;
+        if(ch>='0'&&ch<='9'){
+            push(&S,ch-'0');//把字符转换成数字,因为字符'0'的ASCII码是48，所以减去48就是数字本身
+        }
+        else{
+            a=pop(&S);
+            b=pop(&S);
+            switch(ch){
+                case '+':push(&S,a+b);break;
+                case '-':push(&S,a-b);break;
+                case '*':push(&S,a*b);break;
+                case '/':push(&S,a/b);break;
+            }
+        }
+        RPN++;
+    }
+    return getTop(S);
+}
+int main(){
+    char RPN[100];
+    printf("请输入逆波兰表达式：");
+    scanf("%s",RPN);
+    printf("计算结果为：%d\n",calculateRPN(RPN));
+    return 0;
 }
