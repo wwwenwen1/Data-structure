700707f928e36050bd9b7ec31d20c079ddad2151
dww <710241413@qq.com>
2023-09-19 16:19:21 +0800

    9.19修改对称矩阵压缩算法

--- exercise/text.c
+++ exercise/text.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
-    printf("ERROR,there is no \'(\',before get \')\' in.\n");
+    int rows, cols;
+
+    printf("请输入矩阵的行数: ");
+    scanf("%d", &rows);
+    printf("请输入矩阵的列数: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("请输入矩阵的元素（按行输入，每行元素用空格分隔，回车结束每行）:\n");
+
+    // 逐行读取矩阵元素
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // 打印矩阵
+    printf("您输入的矩阵是:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
